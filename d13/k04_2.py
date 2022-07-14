78176a67d10d45697c1e2d5ccb2683d394cb0bf0
Kotaro SONODA <kotaro1976@gmail.com>
2022-07-15 01:35:43 +0900

    rename k05 to k04

---
+++ d13/k04_2.py
@@ -0,0 +1,20 @@
+# k05_2.py
+
+import amida
+
+kuji = {1:4, 2:3, 3:1, 4:5, 5:2} # {ID:goal}形式のdict
+L = [] # 横棒のリスト
+
+for ID, goal in kuji.items():
+    now = amida.getstate(L, ID) # 既にある横棒Lの操作後における，IDの現在地を返す
+
+    if now == goal: # IDの現在地がすでにgoalにあるならこのifブロックより後の処理を飛ばして次の繰り返しへ
+        continue
+
+    lines = amida.genroutes(now,goal) # IDが現在地nowからgoalに移るまでの横棒リストを返す．
+
+    L.extend(lines) # 既存のリストの後ろにlineを加える
+
+L = amida.reduct(L) # Lの中で連続する同じ横棒を省略する
+
+amida.plot(L,kuji) # あみだくじを描画する
