2d99ecc8e3ebeab2ca33592b5d67cebf0622a731
Kotaro SONODA <kotaro1976@gmail.com>
2022-08-22 14:10:02 +0900

    ...

--- d13/k04_2.py
+++ d13/k04_2.py
@@ -1,6 +1,6 @@
-# k05_2.py
+# k04_2.py
 
-import saegusa_amida as amida
+import amida
 
 kuji = {1:4, 2:3, 3:1, 4:5, 5:2} # {ID:goal}形式のdict
 L = [] # 横棒のリスト
@@ -18,5 +18,3 @@ for ID, goal in kuji.items():
 L = amida.reduct(L) # Lの中で連続する同じ横棒を省略する
 
 amida.plot(L,kuji) # あみだくじを描画する
-
-print(L)
